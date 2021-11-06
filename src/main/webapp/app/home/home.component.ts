72badaf6533d68f36568d7560e53728335003870
ana <anamaria.panait10@yahoo.com>
2021-11-07 01:26:44 +0200

    Added main page table

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -5,6 +5,11 @@ import { takeUntil } from 'rxjs/operators';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { UsersPasswordsService } from '../entities/users-passwords/service/users-passwords.service';
+import { HomeService } from '../home.service';
+import { IUsersPasswords, UsersPasswords } from '../entities/users-passwords/users-passwords.model';
+import { UsersPasswordsDeleteDialogComponent } from '../entities/users-passwords/delete/users-passwords-delete-dialog.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-home',
@@ -12,16 +17,26 @@ import { Account } from 'app/core/auth/account.model';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  isLoading = false;
+  passwordList: UsersPasswords[] = [];
   account: Account | null = null;
   private readonly destroy$ = new Subject<void>();
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private usersPasswordsService: HomeService,
+    protected modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
-      .subscribe(account => (this.account = account));
+      .subscribe(account => {
+        this.account = account;
+        this.loadAll();
+      });
   }
 
   login(): void {
@@ -31,5 +46,23 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  loadAll(): void {
+    this.isLoading = true;
+    this.usersPasswordsService.getAllPasswords().subscribe(r => {
+      this.passwordList = r;
+    });
+  }
+
+  delete(usersPasswords: IUsersPasswords): void {
+    const modalRef = this.modalService.open(UsersPasswordsDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.usersPasswords = usersPasswords;
+    // unsubscribe not needed because closed completes on modal close
+    modalRef.closed.subscribe(reason => {
+      if (reason === 'deleted') {
+        this.loadAll();
+      }
+    });
   }
 }
